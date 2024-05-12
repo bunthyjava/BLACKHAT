640a9a6bba6624d0446d82be9cc5eda8f419a60f
bunthyjava <168361968+bunthyjava@users.noreply.github.com>
2024-05-13 06:55:36 +0700

    Add files via upload

---
+++ BLACKHAT.sh
@@ -0,0 +1,295 @@
+clear
+clear
+mkdir BLACKHAT
+clear 
+echo -e '\033[31;38;1m  
+
+\033[1;31m▄▄▄▄╶╶╶▄▄▌╶╶╶╶▄▄▄╶╶╶▄▄╶╶╶▄╶╶▄╶╶╶▄╶╶▄╶╶▄▄▄╶╶▄▄▄▄▄╶ 
+▐█╶▀█╶╶██╶╶╶╶▐█╶▀█╶▐█╶▌╶╶█▌▄▌╶╶██╶▐█╶▐█╶▀█╶╶██╶╶╶ 
+▐█▀▀█▄╶██╶╶╶╶▄█▀▀█╶██╶▄▄╶▐▀▀▄╶╶██▀▐█╶▄█▀▀█╶╶▐█╶╶╶ 
+██▄╶▐█╶▐█▌▐▌╶▐█╶╶▐▌▐███▌╶▐█╶█▌╶██▌▐▀╶▐█╶╶▐▌╶▐█▌╶╶ 
+╶▀▀▀▀╶╶╶▀▀▀╶╶╶▀╶╶▀╶╶▀▀▀╶╶╶▀╶╶▀╶▀▀▀╶╶╶╶▀╶╶▀╶╶▀▀▀╶╶ 
+               \033[1;31m  @BunthyNetwork 
+       \033[1;33mVersion:\033[0m 1.0           \033[1;36mCTRL+C:\033[31;38;1m exit
+ \033[1;32m┏━━━━━━━━━━━━━━━━━• BLACKHAT •━━━━━━━━━━━━━━━━━┓
+\033[1;31m                ┏━━━━━━━━━━━━━━━━┓
+                      Attack 
+                ┗━━━━━━━━━━━━━━━━┛  
+     \033[1;32m
+                ┏[1]~DDOS ATTACK 
+                ┣[2]~ZPHISHER
+                ┣[3]~LAZYMUX
+                ┣[4]~CCTV HACK
+                ┣[5]~SOCIAL PHISH 
+                ┗[6]~ADVANCED PHISHING
+                ┏[7]~EMAIL-SPAMMER \033[1;31mUpdate \033[1;32m
+                ┣[8]~PYPHISHER
+                ┣[9]~CRACKER TOOL
+                ┣[10]~AUTO REPORT FB
+                ┣[11]~WEBSITE INFORMATION
+                ┗[12]~IPDRONE
+                ┏[13]~NIVOS
+                ┣[14]~RECONDOG
+                ┣[15]~BLUETOOTHDOS
+                ┗[16]~WIFI HACK
+
+\033[1;32m┗━━━━━━━━━━━━━━━━━• BLACKHAT •━━━━━━━━━━━━━━━━━┛
+ \033[1;36m
+'
+
+##Meun Coding
+
+read -p "~ Enter number: " islem
+if [[ $islem == 1 || $islem == 01 ]]; then
+clear
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 2
+pkg update
+pkg upgrade 
+pkg install python3 
+python3-pip git-y
+git clone https://github.com/7zx/overload
+cd overload
+pip install -r requirements.txt
+python3 overload.py
+
+## Phishing Tool
+elif [[ $islem == 2 || $islem == 02 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 2
+cd ZPHISHER-BLACKHAT
+git clone https://github.com/htr-tech/zphisher
+cd zphisher
+bash zphisher.sh
+
+## Lazymux Tool
+elif [[ $islem == 3 || $islem == 03 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux 
+chmod +X *
+python lazymux.py
+
+## CCTV Tool
+elif [[ $islem == 4 || $islem == 04 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+pkg install ruby
+gem install lolcat
+git clone https://github.com/STLP-TEAM/Hack_CC_TV
+pip install requests
+cd Hack_CC_TV
+python hack_cc.py
+
+## Social Phish Tool
+elif [[ $islem == 5 || $islem == 05 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt update -y
+apt upgrade -y
+pkg install git -y
+git clone https://github.com/xHak9x/SocialPhish.git
+cd SocialPhish
+chmod +x socialphish.sh
+./socialphish.sh
+
+## Advanced phishing Tool
+elif [[ $islem == 6 || $islem == 06 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt install python3 curl php git openssh -y
+pip3 install requests wget pyshorteners
+git clone https://github.com/Cyber-Anonymous/Dark-Phish.git
+cd Dark-Phish
+chmod +x  dark-phish.py
+apt install localxpose
+python3 dark-phish.py
+
+## Email-Spammer Tool
+elif [[ $islem == 7 || $islem == 07 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+bash BLACKHAT.sh
+
+## PYPHISHER Tool
+elif [[ $islem == 8 || $islem == 08 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+pkg install git python3 php openssh -y
+git clone https://github.com/KasRoudra/PyPhisher
+cd PyPhisher
+pip3 install -r files/requirements.txt
+python3 pyphisher.py
+Or, directly run
+wget https://raw.githubusercontent.com/KasRoudra/PyPhisher/main/pyphisher.py && python3 pyphisher.py
+
+## Cracker-Tool Tool
+elif [[ $islem == 9 || $islem == 09 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt update -y
+apt upgrade -y
+pkg install git 
+pkg install python -y
+git clone https://github.com/cracker911181/Cracker-Tool
+cd Cracker-Tool
+chmod +x *
+python cracker-main.py
+
+## Auto Report  Tool
+elif [[ $islem == 10 || $islem == 10 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/PangeranAlvins/Repot3
+cd Repot3
+bash install.sh
+python2 Repot3.py
+
+## RED_HAWK Tool
+elif [[ $islem == 11 || $islem == 11 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+cd RED_HAWK
+chmod +x rhawk.php
+apt install php
+php rhawk.php
+
+## ipdrone Tool
+elif [[ $islem == 12 || $islem == 12 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+pip install requests
+pip install requests
+git clone https://github.com/noob-hackers/ipdrone
+cd ipdrone
+python ipdrone.py -v
+
+## NIVOS Tool
+elif [[ $islem == 13 || $islem == 13 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt update 
+apt upgrade
+pkg install git -y
+pkg install python
+pkg install pyhton2
+apt update && upgrade -y 
+pip install os
+git clone https://github.com/theahmadov/NIVOS
+cd NIVOS
+sudo python3 setup.py
+cd NIVOS
+sudo python3 main.py
+
+## ReconDog Tool
+elif [[ $islem == 14 || $islem == 14 ]]; then
+clear
+echo -e "\033[3;35m 
+   ░░█░█ █▀█ █░░ █▀█ ▄▀█ █▀▄ ░░ 
+   ░░█▄█ █▀▀ █▄▄ █▄█ █▀█ █▄▀ ░░ 
+  Installation May Take a Long Time Wait\033[0m"
+sleep 3
+apt update && apt upgrade
+pkg install git -y
+pkg install python -y
+git clone https://github.com/s0md3v/ReconDog
+cd ReconDog
+pip install -r requirements.txt
+python dog
+
+## Requirements 
+echo -e "\033[47;31;5m Updating and Installing Requirements...\033[0m"
+sleep 5
+pkg install git -y
+pkg install python python2 -y
+pkg install pip pip2 -y
+pkg install curl -y
+apt update
+apt upgrade -y
+clear
+echo -e "\033[47;3;35m Update Completed...\033[0m"
+sleep 3
+bash BLACKHAT.sh
+
+## Faulty code
+
+else   
+	clear
+        echo -e '\033[36;40;1m You Entered Incorrect Code'	
+	sleep 1
+	clear 
+	bash BLACKHAT.sh
+fi
+    
